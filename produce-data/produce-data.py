4585da9386bcb9a515f6034f2767562adf9ef0ac
Bas Dorsman <superbas1996@gmail.com>
2023-05-23 10:12:12 +0200

    new hst plots

--- produce-data/produce-data.py
+++ produce-data/produce-data.py
@@ -10,11 +10,12 @@ from astropy_healpix import HEALPix
 import dorado.sensitivity
 import astropy.constants as c
 import astropy.units as u
+import stsynphot as stsyn
 
 ###### .py files in parent directory
 import os
 import sys
-sys.path.insert(0, './')
+sys.path.insert(0, '../')
 from produce_lightcurve import Lightcurve
 from bol_to_band import mag_AB_error
 from parameters import getParameters
@@ -22,11 +23,11 @@ from parameters import getParameters
 # get parameters
 parameters = getParameters(osargs_list=['delay','dist','read_data','redden','optical_delay'])
 
-dist = int(parameters['dist'])
-read_data = parameters['read_data']
-optical_delay = int(parameters['optical_delay']) # hours
-delay = int(parameters['delay']) # hours
-redden = parameters['redden']=='True'
+dist = 40 #int(parameters['dist'])
+read_data = 'kilonova' #parameters['read_data']
+optical_delay = 12 #int(parameters['optical_delay']) # hours
+delay = 0 #int(parameters['delay']) # hours
+redden = False #parameters['redden']=='True'
 # print('redden:',redden)
 
 #### parameters
@@ -51,28 +52,30 @@ elif read_data == 'shock':
 
 distance = dist * u.Mpc
 
-b_NUVD = dorado.sensitivity.bandpasses.NUV_D
-b_D1 = dorado.sensitivity.bandpasses.D1
-b_D2 = dorado.sensitivity.bandpasses.D2
-bs_uv = [b_NUVD, b_D1, b_D2]
-bs_uv_name = ['NUV_D','D1','D2']
+#b_NUVD = dorado.sensitivity.bandpasses.NUV_D
+#b_D1 = dorado.sensitivity.bandpasses.D1
+#b_D2 = dorado.sensitivity.bandpasses.D2
+bp_f225w = stsyn.band('wfc3,uvis1,f225w')
+bp_f336w = stsyn.band('wfc3,uvis1,f336w')
+bs_uv = [bp_f225w, bp_f336w]
+bs_uv_name = ['f225w','f336w']
 
 #D1 and D2 break dust map
-bs_uv = [b_NUVD]
-bs_uv_name = ['NUV_D']
+# bs_uv = [b_NUVD]
+# bs_uv_name = ['NUV_D']
 
 
-b_u = sp.SpectralElement.from_file('./input_files/bands/SLOAN_SDSS.u.dat')
-b_g = sp.SpectralElement.from_file('./input_files/bands/SLOAN_SDSS.g.dat')
-b_r = sp.SpectralElement.from_file('./input_files/bands/SLOAN_SDSS.r.dat')
-b_i = sp.SpectralElement.from_file('./input_files/bands/SLOAN_SDSS.i.dat')
-bs_optical = [b_u, b_g, b_r, b_i]
-bs_optical_name = ['u', 'g', 'r', 'i']
+# b_u = sp.SpectralElement.from_file('../input_files/bands/SLOAN_SDSS.u.dat')
+# b_g = sp.SpectralElement.from_file('../input_files/bands/SLOAN_SDSS.g.dat')
+# b_r = sp.SpectralElement.from_file('../input_files/bands/SLOAN_SDSS.r.dat')
+# b_i = sp.SpectralElement.from_file('../input_files/bands/SLOAN_SDSS.i.dat')
+# bs_optical = [b_u, b_g, b_r, b_i]
+# bs_optical_name = ['u', 'g', 'r', 'i']
 
 
-bs_optical_string  = ''
-for band in bs_optical_name:
-    bs_optical_string += band
+# bs_optical_string  = ''
+# for band in bs_optical_name:
+#     bs_optical_string += band
 
 bs_uv_string  = ''
 for band in bs_uv_name:
@@ -80,15 +83,15 @@ for band in bs_uv_name:
 
 
 # define optical observation time
-t_start = optical_delay
-t_end = int(36)+optical_delay
-t_optical = np.linspace(t_start,t_end,int((t_end-t_start)/12+1))*u.hour
+# t_start = optical_delay
+# t_end = int(36)+optical_delay
+# t_optical = np.linspace(t_start,t_end,int((t_end-t_start)/12+1))*u.hour
 
 
 
 ###### import event data, event number 10 is an example of successful detection
 colnames = ['alpha','alpha1','alpha2','alpha3','alpha4','alpha5','alpha6','amp_order','bandpass','beta','coa_phase','distance','eff_dist_g','eff_dist_h','eff_dist_l','eff_dist_t','eff_dist_v','end_time_gmst','eta','f_final','f_lower','g_end_time','g_end_time_ns','geocent_end_time','geocent_end_time_ns','h_end_time','h_end_time_ns','inclination','l_end_time','l_end_time_ns','latitude','longitude','mass1','mass2','mchirp','numrel_dat','numrel_mode_max','numrel_mode_min','phi0','polarization','process_id','psi0','psi3','simulation_id','source','spin1x','spin1y','spin1z','spin2x','spin2y','spin2z','t_end_time','t_end_time_ns','taper','theta0','v_end_time','v_end_time_ns','waveform','filler']
-events_data = pd.read_csv('./input_files/sim_inspiral table.txt',delimiter=",",names=colnames)
+events_data = pd.read_csv('../input_files/sim_inspiral table.txt',delimiter=",",names=colnames)
 end_times = events_data['geocent_end_time']
 latitudes = events_data['latitude']*u.rad
 longitudes = events_data['longitude']*u.rad
@@ -102,7 +105,7 @@ GW_detection_time = Time(end_times[idx],format='gps').utc
 healpix = HEALPix(nside=32, frame=ICRS())
 target = SkyCoord(longitudes[idx],latitudes[idx])
 target_pixel = healpix.skycoord_to_healpix(target)
-schedule = QTable.read('./input_files/{}.ecsv'.format(idx))
+schedule = QTable.read('../input_files/{}.ecsv'.format(idx))
 fov = FOV.from_rectangle(7.1 * u.deg)
 footprints = [fov.footprint_healpix(healpix, row['center'], row['roll']) for row in schedule]
 schedule['found'] = [target_pixel in footprint for footprint in footprints]
@@ -125,7 +128,7 @@ coord_found = schedule['center'][schedule['found']]
 coord_found = coord_found[0]
 
 
-orbits = 14
+orbits = 100
 start_time = 0.02 #days
 
 for i in range(orbits-1):
@@ -165,7 +168,7 @@ extinction_curves = {}
 
 
 # generate extinction curves
-for b, b_name in zip(bs_uv+bs_optical, bs_uv_name+bs_optical_name):
+for b, b_name in zip(bs_uv, bs_uv_name):#zip(bs_uv+bs_optical, bs_uv_name+bs_optical_name):
     if redden:
         extinction_curves[b_name] = lightcurve_object.get_extinctioncurve(coord_found, b) #for a single detection per orbit
     else:
@@ -180,19 +183,19 @@ for b, b_name in zip(bs_uv, bs_uv_name):
     AB_error[b_name] = mag_AB_error(snrs[b_name])
 
 
-for b, b_name in zip(bs_optical, bs_optical_name):
-    t_data[b_name] = t_optical
-    abmags[b_name] = lightcurve_object.calc_abmags(t_optical,theta,b, b_name,radiation=radiation, extinction=extinction_curves[b_name])
-    snrs[b_name] = lightcurve_object.calc_snrs_optical(t_optical, theta, b, b_name, radiation=radiation, extinction=extinction_curves[b_name])
-    AB_error[b_name] = mag_AB_error(snrs[b_name])
+# for b, b_name in zip(bs_optical, bs_optical_name):
+#     t_data[b_name] = t_optical
+#     abmags[b_name] = lightcurve_object.calc_abmags(t_optical,theta,b, b_name,radiation=radiation, extinction=extinction_curves[b_name])
+#     snrs[b_name] = lightcurve_object.calc_snrs_optical(t_optical, theta, b, b_name, radiation=radiation, extinction=extinction_curves[b_name])
+#     AB_error[b_name] = mag_AB_error(snrs[b_name])
 
 # print(abmags)
 
 # SAVE DATA
-print(f"saving data to ./input_files/data/SNR_fiducial_{read_data}_{dist}Mpc_opticalbands_{bs_optical_string}_uvbands_{bs_uv_string}_{delay}h_delay_redden_{redden}_optical_delay_{optical_delay}.pkl'")
+print(f"saving data to ./input_files/data/SNR_fiducial_{read_data}_{dist}Mpc_uvbands_{bs_uv_string}_{delay}h_delay_redden_{redden}_optical_delay_{optical_delay}.pkl'")
 import pickle
-data = [t_data, abmags, snrs, AB_error, extinction_curve]
-with open(f'./input_files/data/SNR_fiducial_{read_data}_{dist}Mpc_opticalbands_{bs_optical_string}_uvbands_{bs_uv_string}_{delay}h_delay_redden_{redden}_optical_delay_{optical_delay}.pkl', 'wb') as tf:
+data = [t_data, abmags, snrs, AB_error, extinction_curves]
+with open(f'../input_files/data/SNR_fiducial_{read_data}_{dist}Mpc_uvbands_{bs_uv_string}_{delay}h_delay_redden_{redden}_optical_delay_{optical_delay}.pkl', 'wb') as tf:
     pickle.dump(data,tf)
 
 # PLOT DATA
@@ -200,9 +203,10 @@ import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 
 for b_name in bs_uv_name:
-    ax.errorbar(t_UV_data.to_value('day'), abmags[b_name],yerr=AB_error[b_name],label=b_name)
-for b_name in bs_optical_name:
-    ax.errorbar(t_optical.to_value('day'), abmags[b_name],yerr=AB_error[b_name],label=b_name)
+    # ax.errorbar(t_UV_data.to_value('day'), abmags[b_name],yerr=AB_error[b_name],label=b_name)
+    ax.plot(t_UV_data.to_value('day'), abmags[b_name], label=b_name)
+# for b_name in bs_optical_name:
+#     ax.errorbar(t_optical.to_value('day'), abmags[b_name],yerr=AB_error[b_name],label=b_name)
 
 ax.invert_yaxis()
 ax.set_ylabel('Magnitude (AB)')
--- produce_data.sh
+++ produce_data.sh
@@ -1,7 +1,7 @@
 delay=0
 dist=40
-read_data='shock'
-redden='False'
+read_data='kilonova'
+redden='True'
 optical_delay=12
 export delay dist read_data redden optical_delay
 
